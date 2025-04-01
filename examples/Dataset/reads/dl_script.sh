235442cbc244251ae94eda0d9524f33d935d433c
Kasper Thystrup Karstensen <kathka@gmail.com>
2025-04-01 09:30:23 +0200

    Getting rid of Large File Storage dep
    Added a single extra BP at the end of the very first contigs in both samples. The BP is a duplicate of the previous last BP. #6

---
+++ examples/Dataset/reads/dl_script.sh
@@ -0,0 +1,4 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/029/ERR14229029/ERR14229029_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/029/ERR14229029/ERR14229029_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR352/000/ERR3528110/ERR3528110_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR352/000/ERR3528110/ERR3528110_1.fastq.gz
