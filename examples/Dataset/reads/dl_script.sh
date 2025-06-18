63be5a5804e8ccc31a6d0f0bd0449832125ba07f
Gitte Aasbjerg <gina@ssi.dk>
2025-06-18 13:24:10 +0200

    Added Nmenengitidis reads

--- examples/Dataset/reads/dl_script.sh
+++ examples/Dataset/reads/dl_script.sh
@@ -2,3 +2,5 @@ wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/029/ERR14229029/ERR14229029_1
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR142/029/ERR14229029/ERR14229029_2.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR352/000/ERR3528110/ERR3528110_2.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR352/000/ERR3528110/ERR3528110_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR254/086/SRR25448586/SRR25448586_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR254/086/SRR25448586/SRR25448586_2.fastq.gz
