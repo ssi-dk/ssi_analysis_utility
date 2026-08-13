152e238a5d4e7bf8050f6d05484a496ca4b7a839
Kasper Thystrup Karstensen <kathka@gmail.com>
2026-08-13 13:53:29 +0200

    Implement modulo on 10 increments

--- .github/scripts/bump_version.py
+++ .github/scripts/bump_version.py
@@ -17,8 +17,12 @@ if label == "major":
     minor = 0
     patch = 0
 
-elif label == "feature":
+elif label == "minor":
     minor += 1
+    # Determine every tens of minor version increments (10, 20, 30 etc.)
+    if (minor % 10) == 0:
+        # Set patch version to 1 directly
+        patch = 1
     patch = 0
 
 elif label == "patch":
